d12c2863634b0d51193bf38336f5102e31677989
Oğuzhan Aydoğan <negatif25@gmail.com>
2025-07-14 16:10:43 +0300

    no message

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -50,7 +50,7 @@ onMounted(() => {
           class="text-gray-600 placeholder-gray-400 bg-gray-200 rounded-md text-center p-1 mt-3 w-full"
           type="password"
           placeholder="Password"
-          pattern="(?=.*[0-9])(?=.*[A-Za-z])(?=.*\W).{6,}"
+          pattern="(?=.*[0-9])(?=.*[A-Za-z])(?=.*[^a-zA-Z0-9]).{6,}"
           title="Password must be at least 6 characters long and contain at least one number and one special character."
           required
         />

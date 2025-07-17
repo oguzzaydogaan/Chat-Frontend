aa58a6e879a3fd37e55ddb36b5beb57a935ddfea
Oguzhan <negatif25@gmail.com>
2025-07-17 22:23:43 +0300

    no message

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -32,36 +32,100 @@ onMounted(() => {
 </script>
 
 <template>
-  <main class="flex justify-center items-center h-screen px-3">
-    <div class="bg-white grid grid-cols-1 w-full shadow-lg rounded-lg max-w-md p-6 px-10">
-      <h4 class="text-center text-2xl text-gray-700 font-bold">Sign in</h4>
-      <form @submit.prevent="handleSignIn">
-        <input
-          @keypress.space.prevent=""
-          v-model="email"
-          class="text-gray-600 placeholder-gray-400 bg-gray-200 rounded-md text-center p-1 mt-5 w-full"
-          type="email"
-          placeholder="Email"
-          required
-        />
-        <input
-          @keypress.space.prevent=""
-          v-model="password"
-          class="text-gray-600 placeholder-gray-400 bg-gray-200 rounded-md text-center p-1 mt-3 w-full"
-          type="password"
-          placeholder="Password"
-          pattern="(?=.*[0-9])(?=.*[A-Za-z])(?=.*[^a-zA-Z0-9]).{6,}"
-          title="Password must be at least 6 characters long and contain at least one number and one special character."
-          required
-        />
-        <button type="submit" class="bg-blue-500 text-white rounded-md p-1 mt-4 text-center w-full">
-          Sign in
-        </button>
-        <RouterLink to="/register" class="text-blue-500 text-sm mt-1 text-center"
-          >Don't have an account? Sign Up</RouterLink
+  <main>
+    <section class="bg-gray-50 dark:bg-gray-900">
+      <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a
+          href="#"
+          class="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
         >
-        <p id="#error" class="text-red-500 text-sm mt-1 text-center"></p>
-      </form>
-    </div>
+          <img
+            class="w-8 h-8 mr-2"
+            src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+            alt="logo"
+          />
+          Flowbite
+        </a>
+        <div
+          class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700"
+        >
+          <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1
+              class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white"
+            >
+              Sign in to your account
+            </h1>
+            <form class="space-y-4 md:space-y-6" @submit.prevent="handleSignIn">
+              <div>
+                <label
+                  for="email"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Your email</label
+                >
+                <input
+                  @keypress.space.prevent=""
+                  v-model="email"
+                  type="email"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  placeholder="name@company.com"
+                  required
+                />
+              </div>
+              <div>
+                <label
+                  for="password"
+                  class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >Password</label
+                >
+                <input
+                  @keypress.space.prevent=""
+                  v-model="password"
+                  type="password"
+                  placeholder="••••••••"
+                  class="bg-gray-50 border border-gray-300 text-gray-900 rounded-lg focus:ring-blue-600 focus:border-blue-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                  required
+                />
+              </div>
+              <div class="flex items-center justify-between">
+                <div class="flex items-start">
+                  <div class="flex items-center h-5">
+                    <input
+                      id="remember"
+                      aria-describedby="remember"
+                      type="checkbox"
+                      class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+                    />
+                  </div>
+                  <div class="ml-3 text-sm">
+                    <label for="remember" class="text-gray-500 dark:text-gray-300"
+                      >Remember me</label
+                    >
+                  </div>
+                </div>
+                <a
+                  href="#"
+                  class="text-sm font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >Forgot password?</a
+                >
+              </div>
+              <button
+                type="submit"
+                class="w-full text-white bg-blue-600 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+              >
+                Sign in
+              </button>
+              <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet?
+                <RouterLink
+                  to="/register"
+                  class="font-medium text-blue-600 hover:underline dark:text-blue-500"
+                  >Sign up</RouterLink
+                >
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </section>
   </main>
 </template>

3b84f52fead80471a245d10392aa50f6e0b06261
Oğuzhan Aydoğan <negatif25@gmail.com>
2025-08-27 16:21:39 +0300

    no message

--- vue/src/views/Calls.vue
+++ vue/src/views/Calls.vue
@@ -1,11 +1,14 @@
 <script setup>
 import { ChevronLeftIcon, ArrowUpRightIcon, ArrowDownLeftIcon } from '@heroicons/vue/24/outline'
 import axios from '@/plugins/axios'
-import { onMounted, ref } from 'vue'
+import { onMounted, onUnmounted, ref } from 'vue'
 import Loading from '@/components/Loading.vue'
 import { useSocketStore } from '@/stores/socket'
+import { useCallStore } from '@/stores/call'
 
 const socketStore = useSocketStore()
+const callStore = useCallStore()
+
 let userId = localStorage.getItem('userId')
 const calls = ref([])
 const isLoading = ref(false)
@@ -51,11 +54,28 @@ function FindKey(time) {
   return key
 }
 
+async function onAddCallToHistory(event) {
+  console.log(event.detail)
+  calls.value.unshift(event.detail)
+}
+
+function findCallTime(time) {
+  const date = new Date(time)
+  const minutes = date.getMinutes().toString().padStart(2, '0')
+  const hours = date.getHours().toString().padStart(2, '0')
+  return `${hours}:${minutes}`
+}
+
 onMounted(async () => {
   isLoading.value = true
   socketStore.connect()
+  window.addEventListener('add-call-to-history', onAddCallToHistory)
   await getCalls()
   isLoading.value = false
+})
+
+onUnmounted(() => {
+  window.removeEventListener('add-call-to-history', onAddCallToHistory)
 })
 </script>
 
@@ -74,13 +94,21 @@ onMounted(async () => {
     <div class="px-2">
       <div
         v-for="call in calls"
-        class="flex justify-between items-center p-4 border-b hover:bg-green-100 border-gray-300 dark:border-gray-400 dark:hover:bg-gray-700"
+        @click="
+          callStore.startCall(
+            call.caller.id == Number(userId) ? call.callee.id : call.caller.id,
+            call.caller.id == Number(userId) ? call.callee.name : call.caller.name,
+          )
+        "
+        class="flex justify-between items-center p-4 border-b hover:bg-gray-100 border-gray-300 dark:border-gray-400 dark:hover:bg-gray-700"
       >
         <p class="font-bold dark:text-white">
           {{ call.caller.id == Number(userId) ? call.callee.name : call.caller.name }}
         </p>
         <div class="flex justify-center items-center gap-2 text-gray-500 dark:text-gray-500">
-          <p class="text-xs text-center">{{ FindKey(call.callTime) }}</p>
+          <p class="text-xs text-center">
+            {{ FindKey(call.callTime) + ' ' + findCallTime(call.callTime) }}
+          </p>
           <ArrowDownLeftIcon
             v-if="call.caller.id != Number(userId)"
             class="size-3"
